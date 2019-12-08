73f8a467aa57fe507e37bec316f74cf2e53eb3fc
Bruce Berkowicz <berknj@Bruces-MacBook-Pro.local>
2019-12-07 19:30:32 -0500

    Day 3 Part 2 - Circuit board start

--- CircuitBoard.py
+++ CircuitBoard.py
@@ -4,9 +4,23 @@
 class CircuitBoard:
     """Implement a circuit board with 2 wires"""
 
+    def __init__(self, in_wires):
+        self.wire1 = in_wires[0]
+        self.wire2 = in_wires[1]
+
+        # the set of points that each wire visits
+        self.wire1_points, self.wire1_steps = self.trace_wire(self.wire1)
+        self.wire2_points, self.wire2_steps = self.trace_wire(self.wire2)
+
     def trace_wire(self, wire):
-        """Trace the path of the wire"""
+        """Trace the path of the wire
+        Do this by building a set of x-y points visited.
+        For part 2 we need to know how many steps were taken to get to an intersection point.
+        Calculated this by creating a dictionary where the key is the x-y point tuple,
+        and the value is the number of steps taken to get there
+        For part 2 we are returning a tuple: the coords set and the steps dict"""
         wire_coords = set()
+        path_steps = {}
         xpos = 0
         ypos = 0
         step_num = 0
@@ -18,6 +32,7 @@ class CircuitBoard:
                     xpos += 1
                     new_coord = (xpos, ypos)
                     wire_coords.add(new_coord)
+                    path_steps[new_coord] = step_num
             elif path[0].upper() == 'L':
                 wire_len = int(path[1:])
                 for i in range(wire_len):
@@ -25,6 +40,7 @@ class CircuitBoard:
                     xpos -= 1
                     new_coord = (xpos, ypos)
                     wire_coords.add(new_coord)
+                    path_steps[new_coord] = step_num
             elif path[0].upper() == 'U':
                 wire_len = int(path[1:])
                 for i in range(wire_len):
@@ -32,6 +48,7 @@ class CircuitBoard:
                     ypos += 1
                     new_coord = (xpos, ypos)
                     wire_coords.add(new_coord)
+                    path_steps[new_coord] = step_num
             elif path[0].upper() == 'D':
                 wire_len = int(path[1:])
                 for i in range(wire_len):
@@ -39,22 +56,15 @@ class CircuitBoard:
                     ypos -= 1
                     new_coord = (xpos, ypos)
                     wire_coords.add(new_coord)
+                    path_steps[new_coord] = step_num
 
-        return wire_coords
-
-    def __init__(self, in_wires):
-        self.wire1 = in_wires[0]
-        self.wire2 = in_wires[1]
-
-        # the set of points that each wire visits
-        self.wire1_points = self.trace_wire(self.wire1)
-        self.wire2_points = self.trace_wire(self.wire2)
+        return (wire_coords, path_steps)
 
     def man_dist(self, point):
         """Return the Manhatten distance for a given point to the origin"""
         return abs(point[0]) + abs(point[1])
 
-    def shortest_path(self):
+    def closest_intersect(self):
         """Return the wire crossing distance that is closest to the origin"""
         # where did the wires cross?
         crossed = self.wire1_points & self.wire2_points
@@ -68,3 +78,18 @@ class CircuitBoard:
             elif this_dist < manhatten_dist:
                 manhatten_dist = this_dist
         return manhatten_dist
+
+    def shortest_path(self):
+        """Return the fewest number of steps to an intersection"""
+        crossed = self.wire1_points & self.wire2_points
+
+        fewest_steps = None
+        for point in crossed:
+            # how many steps in each wire to get to this point?
+            steps1 = self.wire1_steps[point]
+            steps2 = self.wire2_steps[point]
+            if fewest_steps is None:
+                fewest_steps = steps1 + steps2
+            elif fewest_steps > (steps1+steps2):
+                fewest_steps = steps1 + steps2
+        return fewest_steps
--- day_03.py
+++ day_03.py
@@ -45,7 +45,7 @@ def test1():
     wire2 = ['U7','R6','D4','L4']
 
     cboard = CircuitBoard.CircuitBoard([wire1, wire2])
-    manhatten_dist = cboard.shortest_path()
+    manhatten_dist = cboard.closest_intersect()
 
     print("Shortest distance is {}".format(manhatten_dist))
     print("\nTest 1: expected 6 and got {}".format(manhatten_dist))
@@ -54,7 +54,7 @@ def test1():
     wire4 = ['U62','R66','U55','R34','D71','R55','D58','R83']  # = distance 159
 
     cboard = CircuitBoard.CircuitBoard([wire3, wire4])
-    manhatten_dist = cboard.shortest_path()
+    manhatten_dist = cboard.closest_intersect()
 
     print("Shortest distance is {}".format(manhatten_dist))
     print("\nTest 1: expected 159 and got {}".format(manhatten_dist))
@@ -63,10 +63,41 @@ def test1():
     wire6 = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']  # = distance 135
 
     cboard = CircuitBoard.CircuitBoard([wire5, wire6])
-    manhatten_dist = cboard.shortest_path()
+    manhatten_dist = cboard.closest_intersect()
 
     print("Shortest distance is {}".format(manhatten_dist))
     print("\nTest 1: expected 135 and got {}".format(manhatten_dist))
+
+
+def test2():
+    """Test data for part 2"""
+
+    wire1 = ['R8','U5','L5','D3']
+    wire2 = ['U7','R6','D4','L4']
+
+    cboard = CircuitBoard.CircuitBoard([wire1, wire2])
+    step_cnt = cboard.shortest_path()
+
+    print("\nShortest distance is {}".format(step_cnt))
+    print("Test 2: expected 6 and got {}".format(step_cnt))
+
+    wire3 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
+    wire4 = ['U62','R66','U55','R34','D71','R55','D58','R83']  # = distance 159
+
+    cboard = CircuitBoard.CircuitBoard([wire3, wire4])
+    step_cnt = cboard.shortest_path()
+
+    print("\nShortest distance is {}".format(step_cnt))
+    print("Test 2: expected 159 and got {}".format(step_cnt))
+
+    wire5 = ['R98', 'U47', 'R26', 'D63', 'R33', 'U87', 'L62', 'D20', 'R33', 'U53', 'R51']
+    wire6 = ['U98', 'R91', 'D20', 'R16', 'D67', 'R40', 'U7', 'R15', 'U6', 'R7']  # = distance 135
+
+    cboard = CircuitBoard.CircuitBoard([wire5, wire6])
+    step_cnt = cboard.shortest_path()
+
+    print("\nShortest distance is {}".format(step_cnt))
+    print("Test 2: expected 135 and got {}".format(step_cnt))
 
 
 def part1():
@@ -78,13 +109,19 @@ def part1():
         wire2 = (indat[1]).split(',')
 
         cboard = CircuitBoard.CircuitBoard([wire1, wire2])
-        manhatten_dist = cboard.shortest_path()
+        manhatten_dist = cboard.closest_intersect()
 
         print("Shortest distance is {}".format(manhatten_dist))
 
 
+def part2():
+    """Part 2 - determine the intersection point that requires the fewest total steps to reach"""
+    test2()
+
+
 def main():
     part1()
+    part2()
 
 
 if __name__ == "__main__":
